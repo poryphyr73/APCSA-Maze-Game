c34727036875aa58ea9bd48b0036c0b2825d80c4
poryphyr73 <79426271+poryphyr73@users.noreply.github.com>
2023-01-15 05:27:12 -0400

    0.9
    Make updates to the win trigger and the base of the game is finished.
    
    Consider adding features like doors or enermies?

--- Manager.java
+++ Manager.java
@@ -1,8 +1,91 @@
+import java.util.Scanner;
+
 public class Manager 
 {
+    private static Scanner kb;
+    private static Map m;
+    private static boolean isPlaying = true;
+
+    private static void startNewGame()
+    {
+        int width = 0, height = 0;
+        while(true)
+        {
+            System.out.println("Please input a width and height for your maze (MINIMUM 3)");
+            try{
+                System.out.print("WIDTH: ");
+                width = kb.nextInt();
+                System.out.print("HEIGHT: ");
+                height = kb.nextInt();
+            } catch(Exception e){
+                System.out.println("Invalid input - please try again!\n");
+                kb.nextLine();
+                startNewGame();
+            }
+            if(width >= 3 && height >= 3) break;
+            System.out.println("Invalid size - please try again!\n");
+        }
+        m = new Map(width + (1 - width % 2), height + (1 - height % 2));
+        kb.nextLine();
+        runGame();
+    }
+
+    private static void runGame()
+    {
+        while(isPlaying)
+        {
+            System.out.println("\n"+m);
+            if(m.isFinished()) break;
+            getInput();
+        }
+        String decision = "";
+        
+        while(true && isPlaying)
+        {
+            kb.nextLine();
+            System.out.println("Play again? (Y/N): ");
+            decision = kb.nextLine();
+            if(decision.equalsIgnoreCase("Y") || decision.equalsIgnoreCase("N")) break;
+            System.out.println("Invalid selection - please try again!");
+        }
+        System.out.println();
+        if(decision.equalsIgnoreCase("Y")) startNewGame();
+        System.out.println("Thanks for playing!");
+        kb.close();
+    }
+
+    private static void getInput()
+    {
+        System.out.println("Where would you like to move?");
+        System.out.println("w = UP, d = RIGHT, s = DOWN, a = LEFT, q = QUIT");
+        String input = kb.nextLine() + " ";
+        switch(input.charAt(0))
+        {
+            case 119:
+                m.move(0, 1);
+                break;
+            case 100:
+                m.move(1, 0);
+                break;
+            case 115:
+                m.move(0, -1);
+                break;
+            case 97:
+                m.move(-1, 0);
+                break;
+            case 113:
+                isPlaying = false;
+                break;
+            default:
+                System.out.println("Invalid selection - please try again!");
+                break;
+        }
+    }
+
     public static void main(String[] args)
     {
-        Map m = new Map();
-        System.out.println(m);
+        kb = new Scanner(System.in);
+
+        startNewGame();
     }
 }
--- Map.java
+++ Map.java
@@ -1,41 +1,147 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Map 
 {
-    private int[][] mapTiles = new int[20][20];
-    private char[] signs = {' ', '#', '@'};
-    private int playerX = 0;
-    private int playerY = 0;
+    private char[][] mapTiles; //the current maze. true tiles are empty and walkable; false tiles are walls
 
-    public Map()
+    private int playerX = 0, playerY = 0; //player pointer's position on the x and y axes
+
+    /** Initializes a random maze with given size
+     * 
+     * @param width The width (x) of the requested maze
+     * @param height The height (y) of the requested maze
+     */
+    public Map(int width, int height)
     {
-        init();
+        generateMap(width, height);
     }
 
-    private void init()
+    //#region Map Generation
+
+    /** Initializes the mapTiles array with a new size. Starts the recursive depth-first search algorithm for new map generation
+     * 
+     * @param width The width of the new map. This value must be greater than 2, and it works best with odd numbers for aesthetics
+     * @param height The height of the new map. This value must be greater than 2, and it works best with odd numbers for aesthetics
+     */
+    private void generateMap(int width, int height)
     {
-        for(int i = 0; i < mapTiles.length; i++)
+        mapTiles = new char[height][width];
+        for(int i = 0; i < mapTiles.length; i++) for(int j = 0; j < mapTiles[0].length; j++) mapTiles[i][j] = 'X';
+        int row = 0, col = 0;
+        while(row % 2 == 0) row = (int) (Math.random() * mapTiles.length);
+        while(col % 2 == 0) col = (int) (Math.random() * mapTiles[0].length);
+
+        mapTiles[row][col] = ' ';
+
+        pathfind(row, col);
+
+        mapTiles[mapTiles.length - 2][0] = ' ';
+        mapTiles[1][mapTiles[0].length - 1] = '*';
+        playerY = mapTiles.length - 2;
+    }
+
+    /** The recursive method for pathfinding through the grid to create a new maze
+     * 
+     * @param r The row at which to place the pointer for the next search. Works best with odd numbers, untested with even
+     * @param c The column at which to place the pointer for the next search. Works best with odd numbers, untested with even
+     */
+    private void pathfind(int r, int c)
+    {
+        Integer[] dirs = generateRandomDirArray();
+        System.out.println("Finding new path at ["+r+", "+c+"]");
+
+        for (Integer i : dirs) 
         {
-            mapTiles[i][0] = 1;
-            mapTiles[0][i] = 1;
-            mapTiles[i][mapTiles.length - 1] = 1;
-            mapTiles[mapTiles.length - 1][i] = 1;
+            switch(dirs[i])
+            {
+                case 0:
+                    if(r - 2 > 0 && mapTiles[r - 2][c] != ' ')
+                    {
+                        mapTiles[r-1][c] = ' ';
+                        mapTiles[r-2][c] = ' ';
+                        pathfind(r - 2, c);
+                    }
+                    break;
+                case 1:
+                    if(c + 2 < mapTiles[0].length - 1 && mapTiles[r][c + 2] != ' ')
+                    {
+                        mapTiles[r][c + 1] = ' ';
+                        mapTiles[r][c + 2] = ' ';
+                        pathfind(r, c + 2);
+                    }
+                    break;
+                case 2:
+                    if(r + 2 < mapTiles.length - 1 && mapTiles[r + 2][c] != ' ')
+                    {
+                        mapTiles[r+1][c] = ' ';
+                        mapTiles[r+2][c] = ' ';
+                        pathfind(r + 2, c);
+                    }
+                    break;
+                case 3:
+                    if(c - 2 > 0 && mapTiles[r][c - 2] != ' ')
+                    {
+                        mapTiles[r][c-1] = ' ';
+                        mapTiles[r][c-2] = ' ';
+                        pathfind(r, c - 2);
+                    }
+                    break;
+                default:
+                    System.out.println("DEAD END");
+                    break;
+            }
         }
     }
 
-    public void generateMap()
+    /** Generates a new random order for the cardinal directions of motion for the generative pointer
+     * 
+     * @return An array of Integer types containing values 1-4 in random order
+     */
+    private Integer[] generateRandomDirArray()
     {
-        
+        ArrayList<Integer> dirs = new ArrayList<Integer>();
+        for(int i = 0; i < 4; i++) dirs.add(i);
+        Collections.shuffle(dirs);
+        return dirs.toArray(new Integer[4]);
+    }
+    //#endregion Map Generation
+
+    /** Determines if the player has reached the goal of the maze
+     * 
+     * @return The truth of if the player position matches the end position
+     */
+    public boolean isFinished()
+    {
+        return playerX == mapTiles[0].length - 1 && playerY == 1;
     }
 
+    /** Moves the player pointer throughout the grid only if a suggested position does not fall on a wall
+     * 
+     * @param x The amount to increment the x position of the player. Positive numbers move the pointer right, negative move it left
+     * @param y The amount to increment the y position of the player. Positive numbers move the pointer up, negative move it down
+     */
+    public void move(int x, int y)
+    {
+        if(mapTiles[playerY - y][playerX + x] != 'X')
+        {
+            playerY -= y;
+            playerX += x;
+        }
+        else System.out.println("Invalid position - please try again!");
+    }
+    
     public String toString()
     {
         String ret = "";
-        for(int i = 0; i < mapTiles.length; i++)
+        for(int j = 0; j < mapTiles.length; j++)
         {
-            for(int j = 0; j < mapTiles[i].length; j++)
+            for(int i = 0; i < mapTiles[0].length; i++)
             {
-                ret += signs[mapTiles[j][i]] + " ";
+                if(j == playerY && i == playerX) ret+="@ ";
+                else ret+=mapTiles[j][i]+" ";
             }
-            ret += "\n";
+            ret+="\n";
         }
         return ret;
     }
